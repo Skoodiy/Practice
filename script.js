21736d4fe825f03f145f5787a438591c6133af5a
Kanstantsin Muski <kostikla1010044@gmail.com>
2022-07-11 20:59:41 +0200

    Add if construction and style.css

--- script.js
+++ script.js
@@ -9,8 +9,17 @@ const personlMovieDB = {
 };
 
 let movieRate = function() {
-    let movie = prompt('Напишите название одного из недавно просмотренных фильмов','Titanic');
-    let rate = prompt('На сколько из 10 можете его оценить?','7.7');
+    let movie;
+    let rate;
+    
+    do {
+        movie = prompt('Напишите название одного из недавно просмотренных фильмов','Titanic');
+    } while (!movie || movie.length > 50);
+        
+    do {
+        rate = prompt('На сколько из 10 можете его оценить?','7.7');
+    } while (!rate || rate.length > 50);
+
     personlMovieDB.movies[movie] = rate;
 };
 
@@ -18,4 +27,14 @@ for (let i = 0; i < 2; i++) {
     movieRate();
 }
 
-console.log(personlMovieDB);
+if (personlMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personlMovieDB.count >=10 && personlMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personlMovieDB.count >= 30) {
+    alert('Вы киноман!');
+} else {
+    alert ('Произошла ошибка');
+}
+
+console.log(personlMovieDB.movies);
