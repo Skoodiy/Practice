8f7d586c74757cc5c18bc5ca44d93e91379da11e
Kanstantsin Muski <kostikla1010044@gmail.com>
2022-07-16 17:25:28 +0200

    Add .trim() to all prompt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let numberOfFilms;
 
 function start() {
     do {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','19');
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','19').trim();
     } while (numberOfFilms == false || isNaN(numberOfFilms));
         
     
@@ -23,11 +23,11 @@ function rememberMyFilms() {
     let rate;
     
     do {
-        movie = prompt('Напишите название одного из недавно просмотренных фильмов','Titanic');
+        movie = prompt('Напишите название одного из недавно просмотренных фильмов','Titanic').trim();
     } while (!movie || movie.length > 50);
         
     do {
-        rate = prompt('На сколько из 10 можете его оценить?','7.7');
+        rate = prompt('На сколько из 10 можете его оценить?','7.7').trim();
     } while (!rate || rate.length > 50);
 
     personlMovieDB.movies[movie] = rate;
@@ -62,10 +62,10 @@ function showMyDB() {
 showMyDB();
 
 function writeYourGenres() {
-    const repeatCount = +prompt('Сколько жанров вы хотите добавить?', '3');
+    const repeatCount = +prompt('Сколько жанров вы хотите добавить?', '3').trim();
 
     for (let i = 0; i < repeatCount; i++) {
-        personlMovieDB.genres[i] = prompt(`Ващ людимый жанр под номером ${i + 1}:`,'Фентези');
+        personlMovieDB.genres[i] = prompt(`Ваш людимый жанр под номером ${i + 1}:`,'Фентези').trim();
     }
 }
 
